9513b8b10ff1309a761061ce167ab91f3d7201ed
thatsmedhanashri <dmp282021@gmail.com>
2024-06-29 13:49:08 +0530

    Debugging using ndb

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = require('./app');
 
 const port = process.env.PORT;
 console.log('port - ', port);
-app.listen(port, () => {
+app.listen(9090, () => {
   console.log(`App is running on port ${port}...`);
 });
