df8a8b275c9e7e721ee3cf4cbfcadb22d77c0477
thatsmedhanashri <dmp282021@gmail.com>
2024-07-02 09:23:10 +0530

    Connecting node.js app with mongodb (local & atlas) using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+// mongoose.connect(process.env.DATABASE_LOCAL).then((con) => {
+//   console.log(con);
+//   console.log('DB connection successful!');
+// });
+
+mongoose
+  .connect(
+    'mongodb+srv://Dhanashri:Dhanu%402802@atlascluster.pli1x0n.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster'
+  )
+  .then((con) => {
+    console.log(con);
+    console.log('DB connection successful!');
+  });
 
 const port = process.env.PORT;
 console.log('port - ', port);
